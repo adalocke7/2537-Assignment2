3512c3246dfb5bc7eee821a4532914dff54d2cd3
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-05-11 14:50:48 -0700

    templates and directories set up

--- app.js
+++ app.js
@@ -54,15 +54,16 @@ app.use(session({
 
 // Basic Route
 app.get('/', (req, res) => {
-  if (req.session.authenticated) {
-    res.send(`<h1>Welcome, ${req.session.username}!</h1><a href="/members">Go to Members Area</a> | <a href="/logout">Logout</a>`);
-  } else {
-    res.send('<h1>Home Page</h1><a href="/signup">Signup</a> | <a href="/login">Login</a>');
-  }
+  res.render('home', 
+  { title: 'Home' },
+  {authenticated: req.session.authenticated, username: req.session.username },
+  { errorMessage: '' });
 });
 
 app.get('/signup', (req, res) => {
-  res.send('<h1>Signup Page</h1> <form action="/signingup" method="POST"><input type="text" name="username" placeholder="Username" required><input type="email" name="email" placeholder="Email" required><input type="password" name="password" placeholder="Password" required><button type="submit">Signup</button></form><a href="/login">Already have an account? Login here</a>');
+  res.render('signUp', 
+    { title: 'Signup' },
+    { errorMessage: '' });
 });
 
 app.post('/signingup', async (req, res) => {
@@ -76,20 +77,24 @@ app.post('/signingup', async (req, res) => {
 
   const valuation = schema.validate({ username, email, password });
   if (valuation.error) {
-  console.error(valuation.error.details[0].message); // see exact error
-  res.send(`Error: Incorrect inputted format <a href="/signup">Go back</a>`);
-  return;
+    console.error(valuation.error.details[0].message); // see exact error
+    res.render('signUp', 
+      { title: 'Signup' },
+      { errorMessage: 'Error: Incorrect inputted format' });
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(password, saltRounds);
-  await userCollection.insertOne({username: username, email: email, password: hashedPassword });
+  await userCollection.insertOne({username: username, email: email, password: hashedPassword, user_type: 'user'});
 
   const html = 'Created user successfully! <a href="/login">Login here</a>';
   res.send(html);
 });
 
 app.get('/login', (req, res) => {
-  res.send('<h1>Login Page</h1> <form action="/logingin" method="POST"><input type="email" name="email" placeholder="Email" required><input type="password" name="password" placeholder="Password" required><button type="submit">Login</button></form><a href="/signup">Don\'t have an account? Signup here</a>');
+  res.render('login', 
+    { title: 'Login' },
+    { errorMessage: '' });
 });
 
 app.post('/logingin', async (req, res) => {
@@ -102,14 +107,18 @@ app.post('/logingin', async (req, res) => {
 
   const validationResult = schema.validate({ email, password });
   if (validationResult.error) {
-    res.send('Error: Incorrect inputted format <a href="/login">Go back</a>');
+    res.render('login', 
+      { title: 'Login' },
+      { errorMessage: 'Error: Incorrect inputted format' });
     return;
   }
 
   const result = await userCollection.find({ email: email }).project({email: 1, username: 1, password: 1, _id: 1}).toArray();
 
   if (result.length != 1) {
-    res.send('Error: Invalid email or password <a href="/login">Go back</a>');
+    res.render('login', 
+      { title: 'Login' },
+      { errorMessage: 'Error: Invalid email or password' });
     return;
   }
   if (await bcrypt.compare(password, result[0].password)) {
@@ -120,33 +129,36 @@ app.post('/logingin', async (req, res) => {
     res.redirect('/members');
     return;
   } else {
-    res.send('Error: Invalid email or password <a href="/login">Go back</a>');
+    res.render('login', 
+      { title: 'Login' },
+      { errorMessage: 'Error: Invalid email or password' });
     return;
   }
 });
 
 app.get('/members', (req, res) => {
-  if (!req.session.authenticated) {
-    res.send('Error: Please login first <a href="/login">Login</a>');
-    return;
+  if (req.session.authenticated) {
+    res.render('members', 
+      { title: 'Members Area' },
+      { username: req.session.username,});
   } else {
-    const randomInt = getRandomInt(1, 4);
-    switch(randomInt) {
-      case 1:
-        res.send(`<h1>Members Area</h1><p>Welcome, ${req.session.username}!</p><img src="./duck1.png" alt="Random Image"><a href="/logout">Logout</a>`);
-        break;
-      case 2:
-        res.send(`<h1>Members Area</h1><p>Welcome, ${req.session.username}!</p><img src="./duck2.png" alt="Random Image"><a href="/logout">Logout</a>`);
-        break;
-      case 3:
-        res.send(`<h1>Members Area</h1><p>Welcome, ${req.session.username}!</p><img src="./duck3.png" alt="Random Image"><a href="/logout">Logout</a>`);
-        break;
-      case 4:
-        res.send(`<h1>Members Area</h1><p>Welcome, ${req.session.username}!</p><img src="./duck4.png" alt="Random Image"><a href="/logout">Logout</a>`);
-        break;
-      default:
-        res.send(`<h1>Members Area</h1><p>Welcome, ${req.session.username}!</p><img src="./duck1.png" alt="Random Image"><a href="/logout">Logout</a>`);
-    }
+    res.redirect('/', 
+      { title: 'Home' },
+      { errorMessage: 'Error: You must be logged in to access the members area' }
+    );
+  }
+});
+
+app.get('/admin', (req, res) => {
+  if (req.session.authenticated) {
+    res.render('admin',
+      { title: 'Admin Area' },
+      { username: req.session.username });
+  } else {
+    res.redirect('/', 
+      { title: 'Home' },
+      { errorMessage: 'Error: You must be logged in to an admin account to view this page' }
+    );
   }
 });
 
@@ -156,7 +168,8 @@ app.get('/logout', (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('<h1>404 Not Found</h1>');
+  res.render('404', 
+    { title: '404 Not Found' });
 });
 
 // Start Server
