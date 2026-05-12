a149f2f59c3dfecbfe222598bd612a4f7f06deaa
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-05-11 18:58:55 -0700

    directory fix

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 3000;
 const expireTime = 60 * 60 * 1000; // 1 hour in milliseconds
 
 const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
